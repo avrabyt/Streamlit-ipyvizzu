3f63a6b566e16a74708abadae56f017701ac393f
Avratanu Biswas <avra42@gmail.com>
2022-10-10 00:01:27 +0200

    streamlit-extras implementation test  :zap:

--- pages/BubbleChart.py
+++ pages/BubbleChart.py
@@ -2,49 +2,76 @@ import pandas as pd
 from ipyvizzu import Chart, Data, Config, Style
 from streamlit.components.v1 import html
 import streamlit as st
+from streamlit_extras.dataframe_explorer import dataframe_explorer 
 
+def BubbleChart(df:pd.DataFrame):
+    ''' Create BubbleChart
+    Parameters
+    -----------
+    data_frame : DataFrame
+        The data to display.
+    
+    Returns
+    -----------
+    HTML String
+    '''
+    # Initialize <class 'ipyvizzu.animation.Data'>
+    data = Data()
+    data.add_data_frame(df)
+    # A class for representing a wrapper over Vizzu chart. <class 'ipyvizzu.chart.Chart'>
+    chart = Chart(display="manual")
+    # A method for animating the chart.
+    chart.animate(data)
+    chart.animate(
+        Config(
+            {
+                "channels": {
+                    "color": {"set": ["Joy factors"]},
+                    "size": {"set": ["Value 2 (+)"]},
+                    "label": {"set": ["Country_code"]},
+                },
+                "title": "Bubble Chart",
+                "geometry": "circle",
+            }
+        )
+    )
+    features = [ "Value 2 (+)","Country_code"]
+    chart.animate(
+        Config(
+            {
+                "channels": {
+                    "size": {"set": features}
+                    },
+                    "title": "Stacked Bubble Chart",
+            }
+        ),
+        Style({"plot": {"marker": {"label": {"fontSize": 10}}}}),
+    )
+    return chart._repr_html_()
+
+@st.experimental_memo()
+def load_data(data_path:str):
+    ''' Load the data
+    Parameter
+    ---------
+    data_path : String 
+        Path to Data File
+    Returns
+    -------
+        Pandas.DataFrame
+    ''' 
+    return pd.read_csv(data_path, dtype={"Year": str, "Timeseries": str})
+
+# ------ App ----------
 st.set_page_config(page_title="Streamlit-ipyvizzu", layout="centered")
 st.sidebar.title("Bubble Chart-Demo")
-data_frame = pd.read_csv(
-    "Data/chart_types_eu.csv", dtype={"Year": str, "Timeseries": str}
-)
-with st.expander("Data  ⤵️"):
-    st.dataframe(data_frame)
+data_path = "Data/chart_types_eu.csv"
+df = load_data(data_path=data_path)
 
-# Initialize <class 'ipyvizzu.animation.Data'>
-data = Data()
-data.add_data_frame(data_frame)
-# A class for representing a wrapper over Vizzu chart. <class 'ipyvizzu.chart.Chart'>
-chart = Chart(display="manual")
-# A method for animating the chart.
-chart.animate(data)
-
-chart.animate(
-    Config(
-        {
-            "channels": {
-                "color": {"set": ["Joy factors"]},
-                "size": {"set": ["Value 2 (+)"]},
-                "label": {"set": ["Country_code"]},
-            },
-            "title": "Bubble Chart",
-            "geometry": "circle",
-        }
-    )
-)
-
-
-_t = ["Value 2 (+)", "Country_code"]
-_t = [ "Value 2 (+)","Country_code"]
-chart.animate(
-    Config(
-        {
-            "channels": {"size": {"set": _t}},
-            "title": "Stacked Bubble Chart",
-        }
-    ),
-    Style({"plot": {"marker": {"label": {"fontSize": 10}}}}),
-)
-
-html(chart._repr_html_(), width=700, height=600)
+with st.sidebar:
+    filtered_df = dataframe_explorer(df)
+with st.expander("DataFrame ⤵️"):
+    st.dataframe(filtered_df)
+_CHART = BubbleChart(filtered_df)
+html(_CHART, width=700, height=600)
 st.sidebar.button("Animate ♻️")
