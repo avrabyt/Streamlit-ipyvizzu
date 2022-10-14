51327027fdfe7339bd82ddc8c27cc66f7347a1c4
Avratanu Biswas <avra42@gmail.com>
2022-10-15 00:16:44 +0200

    :zap: basic demo with stvizzu :recycle:

--- pages/Basic-Demo.py
+++ pages/Basic-Demo.py
@@ -1,12 +1,12 @@
-from streamlit.components.v1 import html
+from st_vizzu import *
 import pandas as pd
-from ipyvizzu import Chart, Data, Config, Style
 import ssl
 import streamlit as st 
 from page_config import standard_page_widgets
 from streamlit_extras.mention import mention
 # Add this on top of any page to make mpa-config work!
 standard_page_widgets()
+
 @st.experimental_memo()
 def load_data(data_path:str):
     ''' Load the data
@@ -21,74 +21,6 @@ def load_data(data_path:str):
     return pd.read_csv(data_path,sep=';')
 
 ssl._create_default_https_context = ssl._create_unverified_context  
-# Codes adapted from here 
-# - https://github.com/alod83/data-science/blob/master/DataVisualization/ipyvizzu/ipyvizzu%20Example.ipynb
-
-def create_chart(df):
-    # initialize chart
-    chart = Chart(width="700px", height="600px", display="manual")
-    # add data
-    data = Data()
-    
-    data.add_data_frame(df)
-    
-    chart.animate(data)   
-    Y = ["Country"]
-    X = ["Population (2020)","Subregion"] 
-    # Basic Horizontal Bar
-    chart.animate(
-        Config(
-            {
-                "channels": {
-                    "y": Y,
-                    "x": X,                   
-                }
-            }
-            ),
-            Style(
-                {
-                "plot":{"paddingLeft": "12em"}
-            }
-            ),
-            y = {
-                "duration": 3,   
-        }
-    )
-    
-    chart.animate(
-        Config(
-            {
-                    "y": None,
-                    "x": None,
-                    "size": ["Country","Population (2020)"],
-                    # "size": ["Population (2020)"],
-                    "label": "Country",
-                    "color": "Subregion",
-                    "geometry": "circle"
-            }
-        )
-    )
-    chart.animate(
-        Config(
-            {
-                "y": "Subregion",
-                "x": ["Country","Population (2020)"],
-                "label": None,
-                "size" : None,
-                "geometry": "rectangle"
-            }
-            )        
-    ) 
-    chart.animate(
-        Config(
-            {
-            "x": "Population (2020)",
-            "label": "Population (2020)"
-            }
-            ),
-        )
-    return chart._repr_html_()
-
 # App
 # st.set_page_config(page_title="Streamlit-ipyvizzu", layout="centered")
 st.sidebar.title("Basic-Demo")
@@ -97,9 +29,35 @@ st.sidebar.button("Animate ♻️ ")
 # Load Data
 file_path = 'Data/eu_regions.csv'
 df = pd.read_csv(file_path,sep=';')
-# st.dataframe(df)
-CHART = create_chart(df)
-html(CHART, width=700, height=600)
+
+# Create ipyvizzu Object
+obj = create_vizzu_obj(df)
+config_dict = {"channels": {"y": ["Country"],"x": ["Population (2020)","Subregion"] }}
+style_dict = {"plot":{"paddingLeft": "12em"}}
+# Animate with general dict based arguments 
+anim_obj = vizzu_animate(obj,config_dict=config_dict,style_dict=style_dict)
+# Animate with argument based
+anim_obj1 = beta_vizzu_animate(anim_obj,
+    x=None,y=None,size=["Country","Population (2020)"],
+    label="Country", color="Subregion",geometry="circle")
+# Will use beta vizzu animate when Style reinitializing issue resolved
+anim_obj2 = vizzu_animate(anim_obj1,
+                {
+                "y": "Subregion",
+                "x": ["Country","Population (2020)"],
+                "label": None,
+                "size" : None,
+                "geometry": "rectangle"
+            }
+    
+        )
+anim_obj3 = vizzu_animate(anim_obj2,
+            config_dict={
+            "x": "Population (2020)",
+            "label": "Population (2020)"
+            })
+with st.container():
+    vizzu_plot(anim_obj3,width=800,height=800)
 
 with st.sidebar:
     mention(
@@ -113,5 +71,4 @@ with st.sidebar:
         icon="github",  # Some icons are available... like Streamlit!
         url="https://github.com/vizzuhq/ipyvizzu",
     )
-# from streamlit_extras.app_logo import add_logo
-# add_logo("https://vizzuhq.com/images/icons/ipyvizzu_button.png")
+
