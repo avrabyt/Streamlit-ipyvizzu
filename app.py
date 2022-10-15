fc125e50cf66051bf86d2677c2862231f352274f
avrabyt <avrab.yt@gmail.com>
2022-10-15 10:12:13 +0200

    minor fix in demo app

--- app.py
+++ app.py
@@ -2,24 +2,22 @@ from st_vizzu import *
 import pandas as pd
 import streamlit as st
 
-# st.code("st_vizzu version: " + st_vizzu.__version__)
+st.subheader("Animate plots now with ease!")
 # Load Data
 df = pd.read_csv("Data/music_data.csv", index_col=0)
-with st.expander("Data",expanded=False):
-    df
-
 # Create ipyvizzu Object
 obj = create_vizzu_obj(df)
-# Preset Plots
-# bar_obj = bar_chart(df,
-#             x = "Genres", 
-#             y = "Popularity"
-#             )
+# Preset plot usage
+bar_obj = bar_chart(df,
+            x = "Kinds", 
+            y = "Popularity",
+            title= "1.Using preset plot function `bar_chart()`"
+            )
 # Animate with defined arguments 
-anim_obj = beta_vizzu_animate( obj,
+anim_obj = beta_vizzu_animate( bar_obj,
     x = "Genres",
     y =  ["Popularity", "Kinds"],
-    title = "Animate with beta_vizzu_animate () function",
+    title = "2.Animate with:arg specific `beta_vizzu_animate()`",
     label= "Popularity",
     color="Genres",
     legend="color",
@@ -31,23 +29,20 @@ anim_obj = beta_vizzu_animate( obj,
 _dict = {"size": {"set": "Popularity"}, 
     "geometry": "circle",
     "coordSystem": "polar",
-    "title": "Animate with vizzu_animate () function",
+    "title": "3.Animate with: generic dict-based `vizzu_animate()`",
     }
 
 # Animate with general dict based arguments 
 anim_obj2 = vizzu_animate(anim_obj,_dict)
 # Visualize
 with st.container():
-    st.button("Animate")
-    vizzu_plot(anim_obj2)
-
-    
-# with open(f'README.md', 'r') as f:           
-#     st.markdown(f.read(),unsafe_allow_html=True)
-
+    st.button("Animate ♻️ ")
+    vizzu_plot(anim_obj2)    
+with st.expander("Data",expanded=False):
+    df
+# Updating Readme 
 import glob
 import os
-
 with open(f'README.md', 'r') as f:
     readme_lines = f.readlines()
     readme_buffer = []
