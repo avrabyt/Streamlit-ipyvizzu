7f0cbfa70bdd2a8158f3b4307846a53ae5f01bb3
Avra <96898985+avrabyt@users.noreply.github.com>
2023-08-03 11:18:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from st_pages import Page,show_pages, add_page_title
 
 # Optional -- adds the title and icon to the current page
 # add_page_title()
-
+st.header("👋🏼 Welcome!")
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
 show_pages(
