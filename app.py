bb37e01bb9546c74a67473389a8f2c6d21e3f1aa
Avra <96898985+avrabyt@users.noreply.github.com>
2023-08-03 11:16:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 from st_pages import Page,show_pages, add_page_title
 
 # Optional -- adds the title and icon to the current page
-add_page_title()
+# add_page_title()
 
 # Specify what pages should be shown in the sidebar, and what their titles and icons
 # should be
